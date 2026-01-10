ed28871f28c9fc4cbfa5e48776ec4d9604738e2a
Aryak Tomar <83336458+AryakTomar@users.noreply.github.com>
2026-01-10 19:37:49 +0530

    Delete rest_api directory

--- rest_api/customer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import CustomerAPI
-from rest_api.customer import views
-
-urlpatterns = [
-    path("",views.CustomerAPI.as_view())
-]
