160aad805363ac318ea7dddc06747aeeb10f95d8
Aryak Tomar <83336458+AryakTomar@users.noreply.github.com>
2026-01-10 19:36:55 +0530

    Delete customer directory

--- customer/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Customer
-
-class CustomerSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        models = Customer
-        fields = "__all__"
