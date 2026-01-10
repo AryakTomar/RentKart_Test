9877e6ef3256e1d58d066fcbe4d04c6b53fc5841
Aryak Tomar <83336458+AryakTomar@users.noreply.github.com>
2026-01-10 19:35:58 +0530

    Delete categories directory

--- categories/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Category
-
-class CategorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        models = Category
-        fields = "__all__"
